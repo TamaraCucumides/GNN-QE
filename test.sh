621ff9499b73e947d6ba904126e751083c79fe2c
Tamara Cucumides <tacucumides@uc.cl>
2023-09-05 15:10:02 -0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -6,7 +6,7 @@
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=tacucumides@uc.cl
 #SBATCH --output=/home/tacucumides/GNN-QE/logs/experiments/%A.log
-#SBATCH --gres=gpu:TitanRTX:1
+#SBATCH --gres=gpu:1
 #SBATCH --cpus=8
 #SBATCH --partition=ialab-high
 pwd; hostname; date
