38549b022371a18a23d623e5a6cf3da1a0511cf0
Tamara Cucumides <tacucumides@uc.cl>
2023-09-06 00:17:45 -0300

    Update test.sh to work within storage

--- test.sh
+++ test.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --workdir=/home/tacucumides
+#SBATCH --workdir=/home/tacucumides/storage
 #SBATCH --ntasks=1
 #SBATCH --job-name=gnn-qe
 #SBATCH --nodelist=scylla
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=tacucumides@uc.cl
-#SBATCH --output=/home/tacucumides/GNN-QE/logs/experiments/%A.log
+#SBATCH --output=/home/tacucumides/storage/GNN-QE/logs/experiments/%A.log
 #SBATCH --gres=gpu:TitanRTX:1
 #SBATCH --cpus=8
 #SBATCH --partition=ialab-high
