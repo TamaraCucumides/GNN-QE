ef901a415895a8fd0d6c1deed416d00ffdcf95be
Tamara Cucumides <tacucumides@uc.cl>
2023-09-17 19:35:18 -0300

    Update and rename run_2.sh to finetune.sh

--- finetune.sh
+++ finetune.sh
@@ -1,19 +1,17 @@
-#!/bin/bash
-#SBATCH --workdir=/home/tacucumides
 #SBATCH --ntasks=1
 #SBATCH --job-name=gnn-qe
 #SBATCH --nodelist=scylla
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=tacucumides@uc.cl
-#SBATCH --output=/home/tacucumides/GNN-QE/logs/experiments/%A.log
+#SBATCH --output=/home/tacucumides/storage/GNN-QE/logs/experiments/%A.log
 #SBATCH --gres=gpu:TitanRTX:1
 #SBATCH --cpus=8
 #SBATCH --partition=ialab-high
 pwd; hostname; date
-echo "Only testing"
+echo "Start"
 echo $(pwd)
-cd /home/tacucumides
+cd /home/tacucumides/
 source miniconda3/etc/profile.d/conda.sh
 conda activate gnn-qe
-cd /home/tacucumides/GNN-QE
-python script/run.py -c config/fb15k237_test.yaml --gpus [0] --checkpoint ../2023-08-30-00-32-39/model_epoch_9.pth
+cd /home/tacucumides/storage/GNN-QE
+python script/run.py -c config/fb15k237_finetune.yaml --gpus [0] --checkpoint ../2023-08-30-00-32-39/model_epoch_9.pth
