7d82f0b3ac37cc642ec4255f81535a519bd69b5a
Tamara Cucumides <tacucumides@uc.cl>
2023-09-20 13:57:39 -0300

    Create test-tri-og.sh

---
+++ test-tri-og.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --workdir=/home/tacucumides/storage
+#SBATCH --ntasks=1
+#SBATCH --job-name=gnn-qe
+#SBATCH --nodelist=hydra
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=tacucumides@uc.cl
+#SBATCH --output=/home/tacucumides/storage/GNN-QE/logs/experiments/%A.log
+#SBATCH --gres=gpu:1
+#SBATCH --cpus=8
+#SBATCH --partition=ialab-high
+pwd; hostname; date
+echo "Start"
+echo $(pwd)
+cd /home/tacucumides/
+source miniconda3/etc/profile.d/conda.sh
+conda activate gnn-qe
+cd /home/tacucumides/storage/GNN-QE
+python script/test.py -c config/test.yaml --checkpoint ../2023-09-20-01-30-16/model_epoch_2.pth --gpus [0]
