20dff645fb60dc03ebb6781d74aa03fb442f8649
Tamara Cucumides <tacucumides@uc.cl>
2023-09-24 20:29:24 -0300

    Update train-nell.sh

--- train-nell.sh
+++ train-nell.sh
@@ -6,8 +6,8 @@
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=tacucumides@uc.cl
 #SBATCH --output=/home/tacucumides/storage/GNN-QE/logs/experiments/nell/%A.log
-#SBATCH --gres=gpu:4
-#SBATCH --cpus=8
+#SBATCH --gres=gpu:TitanRTX:1
+#SBATCH --cpus=4
 #SBATCH --partition=ialab-high
 pwd; hostname; date
 echo "Start"
@@ -17,6 +17,6 @@ source miniconda3/etc/profile.d/conda.sh
 conda activate gnn-qe
 cd /home/tacucumides/storage/GNN-QE
 
-#python script/run.py -c config/nell995.yaml --gpus [0]
+python script/run.py -c config/nell995.yaml --gpus [0]
 
-python -m torch.distributed.launch --nproc_per_node=4 script/run.py -c config/nell995.yaml --gpus [0,1,2,3]
+#python -m torch.distributed.launch --nproc_per_node=4 script/run.py -c config/nell995.yaml --gpus [0,1,2,3]
