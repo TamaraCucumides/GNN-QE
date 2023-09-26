c5121eef906dca15b47b3c0f46df5468eac5fdba
Tamara Cucumides <tacucumides@uc.cl>
2023-09-25 21:22:29 -0300

    Create nell-test-og-sa.sh

---
+++ nell-test-og-sa.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --workdir=/home/tacucumides/storage
+#SBATCH --ntasks=1
+#SBATCH --job-name=nell-test-sa
+#SBATCH --nodelist=hydra
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=tacucumides@uc.cl
+#SBATCH --output=/home/tacucumides/storage/GNN-QE/logs/experiments/nell/%A.log
+#SBATCH --gres=gpu:TitanRTX:1
+#SBATCH --cpus=4
+#SBATCH --partition=ialab-high
+pwd; hostname; date
+echo "Start"
+echo $(pwd)
+cd /home/tacucumides/
+source miniconda3/etc/profile.d/conda.sh
+conda activate gnn-qe
+cd /home/tacucumides/storage/GNN-QE
+python script/test.py -c config/nell-test-SA.yaml --checkpoint ../2023-09-24-20-52-21/model_epoch_10.pth --gpus [0]
