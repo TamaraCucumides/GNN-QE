abfd08766aa2c46e72b8a7526e90d996bb77496c
Tamara Cucumides <tacucumides@uc.cl>
2023-09-27 23:49:47 -0300

    Update nell-fulltrain.sh

--- nell-fulltrain.sh
+++ nell-fulltrain.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --workdir=/home/tacucumides/storage
 #SBATCH --ntasks=1
-#SBATCH --job-name=nell-mixed-train
-#SBATCH --nodelist=hydra
+#SBATCH --job-name=nell-mixed-train-v2
+#SBATCH --nodelist=scylla
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=tacucumides@uc.cl
 #SBATCH --output=/home/tacucumides/storage/GNN-QE/logs/experiments/fb15k/%A.log
