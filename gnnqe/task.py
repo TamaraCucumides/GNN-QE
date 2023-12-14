2261f0b316f1f6e200b85059906b6439567971fa
Tamara Cucumides <tacucumides@uc.cl>
2023-12-14 17:27:24 +0100

    Update task.py

--- gnnqe/task.py
+++ gnnqe/task.py
@@ -103,7 +103,7 @@ class LogicalQuery(tasks.Task, core.Configurable):
             easy = easy_answer[i, :].float()
             easy_idx = [i for i, value in enumerate(easy) if value == 1]
             hard = hard_answer[i, :].float()
-            easy_idx = [i for i, value in enumerate(hard) if value == 1]
+            hard_idx = [i for i, value in enumerate(hard) if value == 1]
             predict = round_pred[i, :]
             data_type = self.id2type[type[int(i)]]
             save_to_csv(easy_idx, hard_idx, predict, data_type, folder='data')
