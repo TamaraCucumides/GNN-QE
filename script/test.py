6b4d911c13f786303711bf586583c818cf163e2f
Tamara Cucumides <tacucumides@uc.cl>
2024-01-07 21:10:47 +0100

    Update test.py

--- script/test.py
+++ script/test.py
@@ -14,8 +14,9 @@ from gnnqe import dataset, gnn, model, task, util
 def test(cfg, solver):
     #solver.model.metric = ("mrr", "hits@1", "mape", "spearmanr")
     solver.model.metric = ("mrr", "hits@1", "mape", "spearmanr",
-                           "Precision@0.3", "Precision@0.5",
-                          "Recall@0.3", "Recall@0.5")
+                           "Precision@0.3", "Precision@0.5", "Precision@0.7",
+                          "Recall@0.3", "Recall@0.5", "Recall@0.7",
+                          "Hard-Recall@0.3", "Hard-Recall@0.5", "Hard-Recall@0.7")
     #solver.evaluate("valid") only evaluate on test?
     solver.evaluate("test")
 
