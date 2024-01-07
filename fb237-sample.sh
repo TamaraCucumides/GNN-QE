78b5cef18157e34907c6dd845f7d03a542d39567
Tamara Cucumides <tacucumides@uc.cl>
2024-01-07 18:59:12 +0100

    Update fb237-sample.sh

--- fb237-sample.sh
+++ fb237-sample.sh
@@ -2,7 +2,7 @@
 #SBATCH --workdir=/home/tacucumides/storage
 #SBATCH --ntasks=1
 #SBATCH --job-name=tri-gnn-qe
-#SBATCH --nodelist=hydra
+#SBATCH --nodelist=scylla
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=tacucumides@uc.cl
 #SBATCH --output=/home/tacucumides/storage/GNN-QE/logs/experiments/%A.log
@@ -15,6 +15,6 @@ echo "Start"
 echo $(pwd)
 cd /home/tacucumides/
 source miniconda3/etc/profile.d/conda.sh
-conda activate gnn-qe
+conda activate nbfnet
 cd /home/tacucumides/storage/GNN-QE
 python script/test.py -c config/fb237-100.yaml --checkpoint ../2023-09-21-14-36-32/model_epoch_8.pth --gpus [0]
