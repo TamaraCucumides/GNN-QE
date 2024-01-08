64adc49b85d0d87af2e22a12b5620fca99de596b
Tamara Cucumides <tacucumides@uc.cl>
2024-01-08 01:23:32 +0100

    Update fb237-mix-last.sh

--- fb237-mix-last.sh
+++ fb237-mix-last.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --workdir=/home/tacucumides/storage
 #SBATCH --ntasks=1
-#SBATCH --job-name=tri-gnn-qe
-#SBATCH --nodelist=yodaxico
+#SBATCH --job-name=test-gnnqe-og-og
+#SBATCH --nodelist=scylla
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=tacucumides@uc.cl
 #SBATCH --output=/home/tacucumides/storage/GNN-QE/logs/experiments/%A.log
@@ -13,9 +13,9 @@
 pwd; hostname; date
 echo "Start"
 echo $(pwd)
-cd /home/tacucumides/
+cd /home/tacucumides/storage
 source miniconda3/etc/profile.d/conda.sh
-conda activate gnn-qe
+conda activate nbfnet
 cd /home/tacucumides/storage/GNN-QE
 python script/test.py -c config/fb237-mix-triangle.yaml --checkpoint ../2023-09-21-14-36-32/model_epoch_8.pth --gpus [0]
 
