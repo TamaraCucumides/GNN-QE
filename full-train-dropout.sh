596fb34fc3b07164c9c3984fba5331324c7baf3b
Tamara Cucumides <tacucumides@uc.cl>
2024-01-17 13:08:08 +0100

    Update full-train-dropout.sh

--- full-train-dropout.sh
+++ full-train-dropout.sh
@@ -2,7 +2,7 @@
 #SBATCH --workdir=/home/tacucumides/storage
 #SBATCH --ntasks=1
 #SBATCH --job-name=gnn-qe
-#SBATCH --nodelist=hydra
+#SBATCH --nodelist=scylla
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=tacucumides@uc.cl
 #SBATCH --output=/home/tacucumides/storage/GNN-QE/logs/experiments/%A.log
