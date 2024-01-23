605e05194e6bfddc5b73cf1f87cc1f2fc83d9e03
Tamara Cucumides <tacucumides@uc.cl>
2024-01-23 23:40:55 +0100

    Update test-dropout-375.sh

--- .sh/237/dropout/sample-anchors/test-dropout-375.sh
+++ .sh/237/dropout/sample-anchors/test-dropout-375.sh
@@ -16,4 +16,4 @@ cd /home/tacucumides/storage
 source miniconda3/etc/profile.d/conda.sh
 conda activate nbfnet
 cd /home/tacucumides/storage/GNN-QE
-python script/test.py -c config/fb237-100.yaml --checkpoint ../2024-01-17-09-08-43/model_epoch_10.pth --gpus [0]
+python script/test.py -c config/fb237-100.yaml --checkpoint ../2024-01-22-22-04-04/model_epoch_9.pth --gpus [0]
