191329d6a75f0a47801f42881701a58a50c929da
Tamara Cucumides <tacucumides@uc.cl>
2024-05-15 03:29:02 +0200

    Create test-dropout-375-squares.sh

---
+++ .sh/237/dropout/test-dropout-375-squares.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --workdir=/home/tacucumides/storage
+#SBATCH --ntasks=1
+#SBATCH --job-name=gnnqe-nips-triangle
+#SBATCH --nodelist=scylla
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=tacucumides@uc.cl
+#SBATCH --output=/home/tacucumides/storage/GNN-QE/logs/experiments/%A.log
+#SBATCH --gres=gpu:1
+#SBATCH --cpus=2
+#SBATCH --partition=ialab-high
+pwd; hostname; date
+echo "Start"
+echo $(pwd)
+cd /home/tacucumides/storage
+source miniconda3/etc/profile.d/conda.sh
+conda activate nbfnet
+cd /home/tacucumides/storage/GNN-QE
+python script/test.py -c config/test-squares.yaml --checkpoint ../2024-01-22-22-04-04/model_epoch_9.pth --gpus [0]
