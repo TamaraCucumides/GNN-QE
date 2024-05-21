9a6b76aa9df0dcca70e387517cd3e8bb3d25b610
Tamara Cucumides <tacucumides@uc.cl>
2024-05-22 01:48:39 +0200

    Update test.py

--- script/test.py
+++ script/test.py
@@ -13,7 +13,7 @@ from gnnqe import dataset, gnn, model, task, util
 
 def test(cfg, solver):
     #solver.model.metric = ("mrr", "hits@1", "mape", "spearmanr")
-    solver.model.metric = ("mrr", "hits@1", "hits@10", "mape", "spearmanr",
+    solver.model.metric = ("mrr", "hits@1", "mape", "spearmanr",
                            "Precision@0.3", "Precision@0.5", "Precision@0.7",
                           "Recall@0.3", "Recall@0.5", "Recall@0.7",
                           "Hard-Recall@0.3", "Hard-Recall@0.5", "Hard-Recall@0.7")
