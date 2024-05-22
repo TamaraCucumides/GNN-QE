23f66564a865660da777061bb5e5c2123e7ce5ad
Tamara Cucumides <tacucumides@uc.cl>
2024-05-22 12:19:46 +0200

    Update nell-test-mix-tri.sh

--- .sh/nell/nell-test-mix-tri.sh
+++ .sh/nell/nell-test-mix-tri.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 #SBATCH --workdir=/home/tacucumides/storage
 #SBATCH --ntasks=1
-#SBATCH --job-name=nell-test-sa
+#SBATCH --job-name=gnnqe-nips-square
 #SBATCH --nodelist=hydra
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=tacucumides@uc.cl
-#SBATCH --output=/home/tacucumides/storage/GNN-QE/logs/experiments/nell/%A.log
-#SBATCH --gres=gpu:TitanRTX:1
-#SBATCH --cpus=4
+#SBATCH --output=/home/tacucumides/storage/GNN-QE/logs/experiments/%A.log
+#SBATCH --gres=gpu:1
+#SBATCH --cpus=2
 #SBATCH --partition=ialab-high
 pwd; hostname; date
 echo "Start"
 echo $(pwd)
-cd /home/tacucumides/
+cd /home/tacucumides/storage
 source miniconda3/etc/profile.d/conda.sh
 conda activate nbfnet
 cd /home/tacucumides/storage/GNN-QE
