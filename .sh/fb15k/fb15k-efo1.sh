ca79223d012ed66f1835460385adc637cc468cf7
Tamara Cucumides <tacucumides@uc.cl>
2024-08-27 00:18:07 +0200

    Update fb15k-efo1.sh

--- .sh/fb15k/fb15k-efo1.sh
+++ .sh/fb15k/fb15k-efo1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --chdir=/home/tacucumides/storage
 #SBATCH --ntasks=1
-#SBATCH --job-name=gnnqe-nips-square
+#SBATCH --job-name=gnnqe-log-efo1
 #SBATCH --nodelist=scylla
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=tacucumides@uc.cl
