514a2ab69e1f11500f1d59727305de76222038e7
Tamara Cucumides <tacucumides@uc.cl>
2024-08-27 00:41:17 +0200

    Create fb15k-efo1-og.sh

---
+++ .sh/fb15k/fb15k-efo1-og.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --chdir=/home/tacucumides/storage
+#SBATCH --ntasks=1
+#SBATCH --job-name=gnnqe-log-efo1
+#SBATCH --nodelist=scylla
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=tacucumides@uc.cl
+#SBATCH --output=/home/tacucumides/storage/GNN-QE/logs/experiments/efo1/%A.log
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-gpu=2
+#SBATCH --partition=ialab-high
+pwd; hostname; date
+echo "Start"
+echo $(pwd)
+cd /home/tacucumides/storage
+source miniconda3/etc/profile.d/conda.sh
+conda activate nbfnet
+cd /home/tacucumides/storage/GNN-QE
+python script/test.py -c config/fb15k-efo1.yaml --checkpoint ../2023-09-23-11-55-04/model_epoch_10.pth --gpus [0]
