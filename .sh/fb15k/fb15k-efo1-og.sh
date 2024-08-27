f4cdc34efef865daf0ecaa039f44e0006690e4a4
Tamara Cucumides <tacucumides@uc.cl>
2024-08-27 02:00:04 +0200

    Update fb15k-efo1-og.sh

--- .sh/fb15k/fb15k-efo1-og.sh
+++ .sh/fb15k/fb15k-efo1-og.sh
@@ -2,7 +2,7 @@
 #SBATCH --chdir=/home/tacucumides/storage
 #SBATCH --ntasks=1
 #SBATCH --job-name=gnnqe-log-efo1
-#SBATCH --nodelist=scylla
+#SBATCH --nodelist=yodaxico
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=tacucumides@uc.cl
 #SBATCH --output=/home/tacucumides/storage/GNN-QE/logs/experiments/efo1/%A.log
